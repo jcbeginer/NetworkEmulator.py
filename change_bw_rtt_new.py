5b81e42fb8484ebb14f229c4a24211d3edcb9989
woosung <46365676+jcbeginer@users.noreply.github.com>
2023-10-13 16:01:02 +0900

    Create change_bw_rtt_new.py

---
+++ change_bw_rtt_new.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import subprocess
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Change network conditions for outbound traffic.')
+    parser.add_argument('--interface', type=str, help='The network interface to control.')
+    parser.add_argument('--BW', type=str, help='Bandwidth to set for the interface.')
+    parser.add_argument('--latency', type=str, help='Latency to set for the interface.')
+    args = parser.parse_args()
+    bandwidth = args.BW
+    latency = args.latency
+
+    # Directly apply to the interface for outbound control
+    cmd = f'sudo tc qdisc replace dev {args.interface} root netem rate {bandwidth}mbit delay {latency}ms limit 1800' #실험을 통해서 cellular buffer size가 1500Bytes * 800이라는 걸 알아내서 이렇게 시도!
+
+    print(cmd)
+    subprocess.run(cmd, shell=True, check=True)
+
+if __name__ == "__main__":
+    main()
